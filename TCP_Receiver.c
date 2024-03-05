ad8c32e334b6a7d879064fa4b4ddc0978a2a1fbe
Matan Ziv <91688347+Matankl@users.noreply.github.com>
2024-03-05 23:53:58 +0200

    Add files via upload

--- TCP_Receiver.c
+++ TCP_Receiver.c
@@ -2,68 +2,285 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
+#include <netinet/tcp.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <time.h>
 
-#define PORT 12345
+#include <arpa/inet.h>
 
-//
-void error(const char *msg) {
-    perror(msg);
-    exit(1);
+#define MAXCLIENTS 1
+#define BUFFER_SIZE 1024
+
+// LinkedList to store the time taken to receive the file and the file size each time
+typedef struct Node {
+    size_t fileSize;
+    double timeTaken;
+    struct Node *next;
+} Node;
+
+//new node function
+Node *newNode(size_t fileSize, double timeTaken) {
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->fileSize = fileSize;
+    node->timeTaken = timeTaken;
+    node->next = NULL;
+    return node;
 }
 
-int main() {
-    int sockfd, newsockfd, n;
-    socklen_t clilen;
-    struct sockaddr_in serv_addr, cli_addr;
-    char buffer[1024];
-    FILE *file;
-    clock_t start, end;
-    double cpu_time_used;
+typedef struct LinkedList {
+    Node *head;
+    size_t size;
+} LinkedList;
 
-    // Create socket
+//new linked list function
+LinkedList *newLinkedList() {
+    LinkedList *list = (LinkedList *)malloc(sizeof(LinkedList));
+    list->head = NULL;
+    list->size = 0;
+    return list;
+}
+
+// Function to add a new node to the linked list
+void addNode(LinkedList *list, size_t fileSize, double timeTaken) {
+    Node *node = newNode(fileSize, timeTaken);
+    node->next = list->head;
+    list->head = node;
+    list->size++;
+}
+
+// Get the file size index
+size_t getFileSizeIndex(LinkedList *list, size_t fileSize) {
+    size_t index = 0;
+    Node *current = list->head;
+    while (current != NULL) {
+        if (current->fileSize == fileSize) {
+            return index;
+        }
+        current = current->next;
+        index++;
+    }
+    return -1;
+}
+
+// Get the time taken index
+size_t getTimeTakenIndex(LinkedList *list, double timeTaken) {
+    size_t index = 0;
+    Node *current = list->head;
+    while (current != NULL) {
+        if (current->timeTaken == timeTaken) {
+            return index;
+        }
+        current = current->next;
+        index++;
+    }
+    return -1;
+}
+
+// Function to calculate the average time taken to receive the file
+double averageTime(LinkedList *list) {
+    double sum = 0;
+    Node *current = list->head;
+    while (current != NULL) {
+        sum += current->timeTaken;
+        current = current->next;
+    }
+    return sum / list->size;
+}
+
+// Function to calculate the average bandwidth
+double averageBandwidth(LinkedList *list) {
+    double sum = 0;
+    Node *current = list->head;
+    while (current != NULL) {
+        sum += (current->fileSize / current->timeTaken) * 1000 / (1024 * 1024);
+        current = current->next;
+    }
+    return sum / list->size;
+}
+
+// Function to free the linked list
+void freeList(LinkedList *list) {
+    Node *current = list->head;
+    while (current != NULL) {
+        Node *temp = current;
+        current = current->next;
+        free(temp);
+    }
+    free(list);
+}
+
+
+
+
+
+
+int main(int argc, char *argv[]) {
+
+    printf("TCP Receiver...\n");
+
+    // Variables
+    int sockfd = -1;                            // Socket file descriptor
+    struct sockaddr_in server, client;          // Server address structure to store the client and server addresses
+    socklen_t clientAddrLen = sizeof(client);   // Client address length
+    struct timeList *list = newLinkedList();    // Linked list to store the time taken to receive the file and the file size each time
+
+    // Reset the server and client address structures to 0
+    memset(&server, 0, sizeof(server));
+    memset(&client, 0, sizeof(client));
+
+    printf("Creating TCP socket...\n");
+    // Create a TCP socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        error("Error opening socket");
+    if (sockfd < 0) {
+        perror("Error opening socket");
+        return 1;
+    }
 
-    // Set server details
-    memset(&serv_addr, '0', sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(PORT);
+    // The variable to store the socket option for reusing the server's address.
+    int opt = 1;
 
-    // Bind the socket
-    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
-        error("Error on binding");
+    printf("Setting socket option to reuse server's address...\n");
+
+    // Set the socket option to reuse the server's address.
+    // This is useful to avoid the "Address already in use" error message when restarting the server.
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        perror("setsockopt failed ");
+        close(sockfd);
+        return 1;
+    }
+
+    printf("Setting server details...\n");
+
+    // Set the servers details
+    server.sin_family = AF_INET; // Set the address family to AF_INET (IPv4)
+    server.sin_addr.s_addr = INADDR_ANY; // Set the server's IP address to the local IP address
+    server.sin_port = htons(atoi(argv[1])); // Set the server's port number to the port number passed as an argument
+
+    printf("Binding socket to server's address...\n");
+
+    // Bind the socket to the server's address
+    if (bind(sockfd, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        perror("Error on binding");
+        close(sockfd);
+        return 1;
+    }
+
+    printf("Listening for incoming connections...\n");
 
     // Listen for incoming connections
-    listen(sockfd, 5);
+    if (listen(sockfd, MAXCLIENTS) < 0) {
+        perror("Error on listening");
+        close(sockfd);
+        return 1;
+    }
 
-    clilen = sizeof(cli_addr);
+    printf("Server is listening to incoming connections on port %d...\n", atoi(argv[1]));
+    printf("Accepting incoming connection...\n");
 
-    // Accept connection
-    newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clilen);
-    if (newsockfd < 0)
-        error("Error on accept");
+    size_t fileSize;
 
-    // Receive file size first
-    long fileSize;
-    read(newsockfd, &fileSize, sizeof(fileSize));
+    while (1) {
+    // Accept the incoming connection
+    int clientSock = accept(sockfd, (struct sockaddr *)&client, &clientAddrLen);
 
-    // Receive file content
-    file = fopen("received_file.txt", "w");
-    start = clock();
-    while ((n = read(newsockfd, buffer, sizeof(buffer))) > 0)
-        fwrite(buffer, 1, n, file);
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    if (clientSock < 0) {
+        perror("Error on accept");
+        close(sockfd);
+        return 1;
+    }
 
-    printf("Received file in %f seconds\n", cpu_time_used);
+    printf("Client connected from %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
 
-    fclose(file);
-    close(newsockfd);
+    // Step 3: Receive the file size
+    ssize_t bytesRead = recv(clientSock, &fileSize, sizeof(fileSize), 0);
+
+    if (bytesRead != sizeof(fileSize)) {
+        perror("Error receiving file size");
+        close(clientSock);
+        close(sockfd);
+        return 1;
+    }
+
+    printf("File size received: %zu bytes.\n", fileSize);
+
+    // Step 3 (Continued): Receive the file and measure the time
+    clock_t start_time, end_time;
+    double time_taken;
+
+    char buffer[BUFFER_SIZE];
+    size_t totalBytesReceived = 0;
+
+    start_time = clock();
+
+    while (totalBytesReceived < fileSize) {
+        bytesRead = recv(clientSock, buffer, sizeof(buffer), 0);
+
+        if (bytesRead <= 0) {
+            perror("Error receiving file data");
+            close(clientSock);
+            close(sockfd);
+            return 1;
+        }
+
+        totalBytesReceived += bytesRead;
+    }
+
+    end_time = clock();
+    time_taken = ((double)(end_time - start_time)) / CLOCKS_PER_SEC * 1000; // Convert to milliseconds
+    addNode(list, fileSize, time_taken);
+
+    printf("File received in %.2f milliseconds.\n", time_taken);
+        // Step 4: Wait for Sender response
+        char response[BUFFER_SIZE];
+        printf("Waiting for Sender response...\n");
+
+        bytesRead = recv(clientSock, response, sizeof(response), 0);
+
+        if (bytesRead > 0) {
+            if (strcmp(response, "resend") == 0) {
+                // Sender wants to resend the file, go back to Step 3
+                printf("Sender resending the file...\n");
+                close(clientSock);
+                continue;
+            } else if (strcmp(response, "exit") == 0) {
+                // Sender sends exit message, go to Step 5
+                printf("Sender sent exit message. Exiting...\n");
+                break;
+            }
+        } else if (bytesRead == 0) {
+            // Connection closed by Sender
+            printf("Connection closed by Sender. Exiting...\n");
+            break;
+        } else {
+            perror("Error receiving Sender response");
+            close(clientSock);
+            break;
+        }
+    }
+
+    // Step 5: Print out the times and average bandwidth
+    printf("----------------------------------\n");
+    printf("- * Statistics * -\n");
+    printf("- Run #1 Data: Time=%.2fms; Speed=%.2fMB/s\n", time_taken, (fileSize / time_taken) * 1000 / (1024 * 1024));
+    printf("-\n");
+    printf("- Average time: %.2fms\n", time_taken);
+    printf("- Average bandwidth: %.2fMB/s\n", (fileSize / time_taken) * 1000 / (1024 * 1024));
+    printf("----------------------------------\n");
+
+    // (Steps 6 and 7 are not applicable in this implementation)
+
     close(sockfd);
 
     return 0;
 }
+
+
+
+//TODO: add linked list to store the time taken to receive the file each time
+//TODO: Add specific congestion control algorithm determined by the argv[4]
+//TODO: in the printing erea add the average time taken to receive the file and the average bandwidth of the file and print all the times spartly 
+//TODO:
--- TCP_Sender.c
+++ TCP_Sender.c
@@ -2,56 +2,199 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <netinet/tcp.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+
 #include <arpa/inet.h>
 
 #define PORT 12345
-#define FILE_SIZE_MB 2
+#define FILE_SIZE_MB 3
+#define FILE_NAME "random_file.txt"
+#define BUFFER_SIZE 1024
 
-void error(const char *msg) {
-    perror(msg);
-    exit(1);
+/*
+    * @brief Create a file with random data.
+    * @param fileName The name of the file to create.
+    * @param fileSizeInMB The size of the file to create in MB.
+    * @return A pointer to the created file.
+    
+*/
+FILE* createRandomCharFile(const char* fileName, size_t fileSizeInMB) {
+    // Open the file for writing, create it if it doesn't exist, truncate it if it does
+    FILE* file = fopen(fileName, "wb");
+    
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(1);
+    }
+
+    // Seed for random data
+    srand(time(NULL));
+
+    // Write random data to the file
+    char buffer[BUFFER_SIZE];
+    size_t remainingBytes = fileSizeInMB * 1024 * 1024;
+
+    while (remainingBytes > 0) {
+        size_t charsToWrite = sizeof(buffer) < remainingBytes ? sizeof(buffer) : remainingBytes;
+
+          for (size_t i = 0; i < charsToWrite; ++i) {
+            buffer[i] = 'A' + rand() % 26;  // Generate a random uppercase letter
+        }
+
+        fwrite(buffer, 1, charsToWrite, file);
+
+        remainingBytes -= charsToWrite;
+    }
+
+    fclose(file);
+
+    return file;
 }
 
-int main() {
-    int sockfd, n;
-    struct sockaddr_in serv_addr;
+
+
+/*
+ * @brief TCP Client main function.
+ * @param argc The number of command-line arguments.
+ * @param argv The command-line arguments ( 1-2 ip address, 3-4 port, 5-6 congestion control algorithm).
+ * @return 0 if the client runs successfully, 1 otherwise.
+*/
+int main( int argc, char *argv[]) {
+
+    printf("TCP Sender...\n");
+
+    //Variables
+    int sockfd = -1;
+    char* Server_IP = argv[2];
     char buffer[1024];
     FILE *file;
 
-    // Create socket
+
+
+    printf("Creating file with random data...\n");
+
+    // Create a file with random data
+    file = createRandomCharFile(FILE_NAME, FILE_SIZE_MB);
+
+
+
+
+
+
+
+    printf("Creating TCP socket...\n"); 
+
+    // Try to create a TCP socket (IPv4, stream-based, default protocol).
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
-        error("Error opening socket");
+        perror("Error opening socket");
+        return 1;
+
+
+
+    printf("Setting server details...\n");
 
     // Set server details
+    struct sockaddr_in serv_addr;
+    // The memset() function is used to fill a block of memory with a particular value. (in this case to reset the serv_addr structure to 0)
     memset(&serv_addr, '0', sizeof(serv_addr));
+    // Store the server details in the serv_addr structure.
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_port = htons(argv[4]);
 
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
-        error("Invalid address");
+
+
+    printf("Converting IP address from text to binary...\n");
+
+    // Convert the server's address from text to binary form and store it in the server structure.
+    // This should not fail if the address is valid.
+    if (inet_pton(AF_INET, Server_IP, &serv_addr.sin_addr) <= 0)
+        perror("Invalid address/ Address not supported");
+        close(sockfd);
+        return 1;
+
+
+
+    printf("Connecting to server...\n");
 
     // Connect to server
     if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
-        error("Error connecting");
+        perror("Connection Failed");
+        close(sockfd);
+        return 1;
 
-    // Generate and send file
-    file = fopen("random_file.txt", "w");
-    // Generate your random file content here
 
-    // Send file size first
-    fseek(file, 0, SEEK_END);
-    long fileSize = ftell(file);
-    rewind(file);
-    write(sockfd, &fileSize, sizeof(fileSize));
 
-    // Send file content
-    while ((n = fread(buffer, 1, sizeof(buffer), file)) > 0)
-        write(sockfd, buffer, n);
 
+    printf("Setting congestion control algorithm...\n");
+
+     // Define congestion control algorithm
+    if (strcmp(argv[6], "reno") == 0) {
+        if (setsockopt(sockfd, IPPROTO_TCP, TCP_CONGESTION, "reno", strlen("reno")) < 0)
+            perror("Error setting congestion control algorithm");
+    } else if (strcmp(argv[6], "cubic") == 0) {
+        if (setsockopt(sockfd, IPPROTO_TCP, TCP_CONGESTION, "cubic", strlen("cubic")) < 0)
+            perror("Error setting congestion control algorithm");
+    } else {
+        perror("Invalid congestion control algorithm");
+        close(sockfd);
+        return 1;
+    }
+    
+
+    // Loop to send the file content agin if the user wants to send the file content again (enter y = yes, n = no)
+    char choice = 'y';          // User's choice
+    int n;                      // Number of bytes read from the file
+
+    do
+    {
+        printf("Sending file size...\n");
+
+        // Send file size first so the reciver knows how much data to expect
+        // The fseek() function is used to move the file pointer to a specified position. In this case, it moves the file pointer to the end of the file.
+        fseek(file, 0, SEEK_END);
+        long fileSize = ftell(file);
+        rewind(file);
+        // The write() function is used to write data to a file descriptor. In this case, it writes the file size to the socket.
+        write(sockfd, &fileSize, sizeof(fileSize));
+
+
+        printf("Sending file content...\n");
+
+        // Send file content using chunks of 1024 bytes at a time until the end of the file is reached. 
+        // wait for the receiver to acknowledge the receipt of the segment before sending the next segment. 
+        while ((n = fread(buffer, 1, sizeof(buffer), file)) > 0)
+            write(sockfd, buffer, n);
+            
+
+        printf("Do you want to send the file content again? (y/n): ");
+        scanf(" %c", &choice);
+    } while (choice == 'y' || choice == 'Y');
+
+
+    printf("Sending exit message...\n");
+
+   // Send exit message
+    write(sockfd, "exit", 4);
+
+
+    // Close the file and the socket
+    printf("Closing file and socket...\n");
     fclose(file);
     close(sockfd);
 
     return 0;
 }
+
+
+
+
+
+// TODO: Add the following to the Makefile
+// TODO: make sure the argv[] are received correctly (port Ip address and congestion control algorithm)
